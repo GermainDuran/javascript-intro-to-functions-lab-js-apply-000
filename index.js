c7a2c3936420c5c600f71493e39e97d278c79172
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-09-03 21:52:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ function logWhisper (string) {
   var aux = string.toLowerCase()
   console.log(aux);
 }
+
 function sayHiToGrandma(string){
   
  string.toLowerCase() === string 
     {return ("I can't hear you!")
     }
-}
-function sayHiToGrandma(string){
+
+
  string.toUpperCase() === string
     {return ("YES INDEED!")
     } 
-}    
- function sayHiToGrandma(string){   
-   string === "I love you, Grandma."
-    return ("I love you, too.")
+    
   
+ string === "I love you, Grandma."
+    return ("I love you, too.")
 }
+
