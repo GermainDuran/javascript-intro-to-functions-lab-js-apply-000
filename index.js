bbd648105e8b48d0356c15716d1a2f8fb36a16cc
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-09-03 20:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function logWhisper (string) {
   console.log(string.toLowerCase)
 }
 function sayHiToGrandma(string){
-
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase() === uppercase // true
+ 
+  var lowercase = 'hello!'
+  lowercase.toLowerCase() === lowercase // true
+ 
+  var mixedCase = 'Hi there!'
+  mixedCase.toLowerCase() === mixedCase // false
+  mixedCase.toUpperCase() === mixedCase // false
   
 }
